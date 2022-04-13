fe40023609d6ce769535c972c11863733d48205e
srilakshmireact <srilakshmi.react11@gmail.com>
2022-04-13 20:23:00 +0530

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import PageNotFound from './components/PageNotFound';
+import Login from './components/Login';
+import Home from './components/Home';
+import DSU from './components/DSU';
+import Tasks from './components/Tasks';
+import Messages from './components/Messages';
+import Requests from './components/Requests';
+import Leaves from './components/Leaves';
+import ManageTasks from './components/ManageTasks';
+import Presantation from './components/Presantation';
+import TaskReview from './components/TaskReview';
+import CreateTask from './components/CreateTask';
+import ClassAttendence from './components/ClassAttendence';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+       
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route>
+        <Route path="home" element={<Home/>}></Route>
+        <Route path="dsu" element={<DSU/>}></Route>
+        <Route path="tasks" element={<Tasks/>}></Route>
+        <Route path="messages" element={<Messages/>}></Route>
+        <Route path="requests" element={<Requests/>}></Route>
+        <Route path="leaves" element={<Leaves/>}></Route>
+        <Route path="*" element={<PageNotFound/>}></Route>
+        <Route path="manageTasks" element={<ManageTasks/>}>
+        
+        <Route path="presentation" element={<Presantation/>}></Route>
+        <Route path="taskReview" element={<TaskReview/>}></Route>
+        <Route path="createTasks" element={<CreateTask/>}></Route>
+        <Route path="classAttendence" element={<ClassAttendence/>}></Route></Route>
+        <Route path="taskReview/:userName" element={<TaskReview/>}></Route>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
