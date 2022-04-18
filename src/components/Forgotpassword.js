2756ed61827ca61fe46a8eafb67f7599fabdefcd
Shohrat Husain <shohrathusain7355@gmail.com>
2022-04-18 13:05:12 +0530

    added css

--- src/components/Forgotpassword.js
+++ src/components/Forgotpassword.js
@@ -46,7 +46,7 @@ function Forgotpassword() {
       <br></br><br></br>
 
      <div className='tag'>
-     <a href="https://www.brninfotech.com/">2022 © BRN Infotech Pvt. Ltd. </a>
+     <a href="https://www.brninfotech.com/" style={{color: "white"}}>2022 © BRN Infotech Pvt. Ltd. </a>
      </div>
      </center>
      <br></br>
