c712f06b7f3206f6f1aed9f3062489fb6ad06b57
Harshika <harshikabhaskar@gmail.com>
2022-04-23 01:04:22 +0530

    added Holiday Task

--- src/components/AdminManage.js
+++ src/components/AdminManage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import AdminTopNavigation from "./AdminTopNavigation";
+import Holiday from "./Holiday";
 
 function AdminManage() {
   return (
     <div>
       <AdminTopNavigation />
+      <Holiday/>
+
       </div>
   
   );
